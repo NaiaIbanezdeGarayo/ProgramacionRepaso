72cdbe78ee1bf8d924c26653372b3a7a733494dc
Naia <naia.idgalo@gmail.com>
2019-05-13 13:43:58 +0200

    EJERCICIO 7 ACABADO

--- Ejercicio7ArrayList/src/ejercicio7arraylist/Ejercicio7ArrayList.java
+++ Ejercicio7ArrayList/src/ejercicio7arraylist/Ejercicio7ArrayList.java
@@ -14,12 +14,10 @@ import javax.swing.JOptionPane;
  */
 public class Ejercicio7ArrayList {
 
-    /**
-     * @param args the command line arguments
-     */
+    static ArrayList<Integer> listaNumeros ;
     public static void main(String[] args) {
        
-        ArrayList<Integer> listaNumeros = new ArrayList<Integer>();
+        listaNumeros = new ArrayList<Integer>();
         int continuar ;
         do {
             int numeros = Integer.parseInt(JOptionPane.showInputDialog("Introduce un número")); 
@@ -35,9 +33,68 @@ public class Ejercicio7ArrayList {
                                             + "4. Eliminar un número.\n"
                                             + "5. Insertar un número en una posición concreta.");
         switch(menu){
-            case "1": 
+            case "1": agregarNumero(); 
+                break;
+            case "2": buscarNumero();
+                break;
+            case "3": modificarNumero();
+                break;
+            case "4": eliminarNumero();
+                break;
+            case "5": insertarNumeroEnPosicion();
+                break;
         
         }
     }
+
+    private static void agregarNumero() {
+        int numero = Integer.parseInt(JOptionPane.showInputDialog("Introduce el número que quieres añadir"));
+        listaNumeros.add(numero);
+    }
+
+    private static void buscarNumero() {
+        int numero = Integer.parseInt(JOptionPane.showInputDialog("Introduce el número que quieres buscar"));
+        boolean buscar = listaNumeros.contains(numero);
+        if ( buscar == true) {
+            JOptionPane.showMessageDialog(null, "El número introducido ha sido encontrado " + numero);
+        }
+        else
+            JOptionPane.showMessageDialog(null, "El número introducido no ha sido introducido");
+    }
+
+    private static void modificarNumero() {
+        int posicion = Integer.parseInt(JOptionPane.showInputDialog("Introduce la posicion que quieres modificar"));
+        int numero = Integer.parseInt(JOptionPane.showInputDialog("Introduce por el número que quieres cambiar"));
+        
+        listaNumeros.set(posicion, numero);
+    }
+
+    private static void eliminarNumero() {
+        String cadena =" ";
+        for (int i = 0; i < listaNumeros.size(); i++) {
+            int numero = listaNumeros.get(i);
+            cadena= cadena + numero + " ";
+        }
+            
+        
+        int posicion  = Integer.parseInt(JOptionPane.showInputDialog("Introduce la posicion que quieres borrar" + "\n"+cadena));
+        listaNumeros.remove(posicion);
+        
+        for (int i = 0; i < listaNumeros.size(); i++) {
+            int numero = listaNumeros.get(i);
+            cadena= cadena + numero + " ";
+        }
+        JOptionPane.showMessageDialog(null, cadena);
+    }
+
+    private static void insertarNumeroEnPosicion() {
+        int posicion = Integer.parseInt(JOptionPane.showInputDialog("Introduce la posicion que quieres insertar el numero"));
+        int numero = Integer.parseInt(JOptionPane.showInputDialog("Introduce el número que quieres insertar"));
+        
+        listaNumeros.add(posicion, numero);
+    }
+
+    
+
     
 }
