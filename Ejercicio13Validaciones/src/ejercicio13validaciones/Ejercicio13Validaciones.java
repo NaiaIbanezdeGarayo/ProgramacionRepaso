02dda8315b63bc6208ffd3fb664535a0adfb6df7
Naia <naia.idgalo@gmail.com>
2019-05-14 10:53:21 +0200

    Ejercicio 13

--- Ejercicio13Validaciones/src/ejercicio13validaciones/Ejercicio13Validaciones.java
+++ Ejercicio13Validaciones/src/ejercicio13validaciones/Ejercicio13Validaciones.java
@@ -5,6 +5,10 @@
  */
 package ejercicio13validaciones;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Naia
@@ -15,7 +19,47 @@ public class Ejercicio13Validaciones {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+        JOptionPane.showMessageDialog(null, "A continuación introduciras tu dirección");
+        //Calle
+        String calle = JOptionPane.showInputDialog("Introduce la calle");
+        Pattern pat = Pattern.compile("^[A-Z][a-z]{2,}");
+        Matcher mat = pat.matcher(calle);
+        if (mat.matches()) {
+            JOptionPane.showMessageDialog(null, "La calle es correcta");
+        }else
+            JOptionPane.showMessageDialog(null, "La calle es incorrecta");
+        
+        //Numero
+        String numero = JOptionPane.showInputDialog("Introduce la calle");
+        Pattern patt = Pattern.compile("^[0-9]{1,3}");
+        Matcher matc = patt.matcher(numero);
+        if (matc.matches()) {
+            JOptionPane.showMessageDialog(null, "La calle es correcta");
+        }else
+            JOptionPane.showMessageDialog(null, "La calle es incorrecta");
+        
+        
+        //Piso
+        String piso = JOptionPane.showInputDialog("Introduce la calle");
+        Pattern pa = Pattern.compile("^^[0-9]{1,2}");
+        Matcher ma = pa.matcher(piso);
+        if (ma.matches()) {
+            JOptionPane.showMessageDialog(null, "La calle es correcta");
+        }else
+            JOptionPane.showMessageDialog(null, "La calle es incorrecta");
+        
+        //Letra
+        String letra = JOptionPane.showInputDialog("Introduce la calle");
+        Pattern p = Pattern.compile("^^[0-9]{1,2}");
+        Matcher m = p.matcher(letra);
+        if (m.matches()) {
+            JOptionPane.showMessageDialog(null, "La calle es correcta");
+        }else
+            JOptionPane.showMessageDialog(null, "La calle es incorrecta");
+        
+        
+        
+        }
+    
     
 }
