8a1feb21a9f45f06205ec5ddbccfa784d9f80ac8
Naia <naia.idgalo@gmail.com>
2019-05-17 14:07:40 +0200

    Ejercicio 17

---
+++ Ejercicio17Clases/src/ejercicio17clases/Pago.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio17clases;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author Naia
+ */
+public class Pago {
+
+    private LocalDate fechaPago;
+    private int importe;
+    private String direccionFacturacion;
+    
+}
